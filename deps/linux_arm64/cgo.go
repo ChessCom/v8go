1508557aff32db08057152c5eec3b5f93fc188ff
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2026-05-19 19:55:07 -0300

    fix: use direct archive paths in LDFLAGS for linker compat
    GNU ld on Ubuntu rejects llvm-ar's 64-bit symbol table format
    when using -L/-l flags ('skipping incompatible'). Pass archives
    as direct paths instead, which forces the linker to read them
    regardless of symbol table format.

--- deps/linux_arm64/cgo.go
+++ deps/linux_arm64/cgo.go
@@ -1,6 +1,6 @@
 package linux_arm64
 
-// #cgo LDFLAGS: -pthread -L${SRCDIR}
-// #cgo LDFLAGS: -lv8-0 -lv8-1 -lv8-2
-// #cgo linux LDFLAGS: -ldl
+// #cgo LDFLAGS: -pthread
+// #cgo LDFLAGS: ${SRCDIR}/libv8-0.a ${SRCDIR}/libv8-1.a ${SRCDIR}/libv8-2.a
+// #cgo LDFLAGS: -ldl -lm -lstdc++
 import "C"

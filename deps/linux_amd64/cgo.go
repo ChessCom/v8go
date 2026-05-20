b1e18aa0330b92cb75a834736b5bd9c76f0fa8fe
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2026-05-20 01:45:00 -0300

    fix: gcc-built V8 + --start-group for link order resolution
    Use gcc-built V8 archives (GNU ld compatible) with
    --start-group/--end-group to resolve cross-archive dependencies.
    Remove lld workaround.

--- deps/linux_amd64/cgo.go
+++ deps/linux_amd64/cgo.go
@@ -1,6 +1,6 @@
 package linux_amd64
 
 // #cgo LDFLAGS: -pthread
-// #cgo LDFLAGS: ${SRCDIR}/libv8-0.a ${SRCDIR}/libv8-1.a ${SRCDIR}/libv8-2.a
+// #cgo LDFLAGS: -Wl,--start-group ${SRCDIR}/libv8-0.a ${SRCDIR}/libv8-1.a ${SRCDIR}/libv8-2.a -Wl,--end-group
 // #cgo LDFLAGS: -ldl -lm -lstdc++
 import "C"

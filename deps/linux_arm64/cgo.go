035680684e71adb56014828efd815a6c7ba957a9
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2026-05-19 23:41:38 -0300

    fix: use CGO_LDFLAGS env var for lld (cgo rejects -fuse-ld in directives)
    CGo's security policy rejects -fuse-ld=lld in #cgo LDFLAGS.
    Set it via CGO_LDFLAGS environment variable in CI instead.

--- deps/linux_arm64/cgo.go
+++ deps/linux_arm64/cgo.go
@@ -1,6 +1,6 @@
 package linux_arm64
 
-// #cgo LDFLAGS: -pthread -fuse-ld=lld
+// #cgo LDFLAGS: -pthread
 // #cgo LDFLAGS: ${SRCDIR}/libv8-0.a ${SRCDIR}/libv8-1.a ${SRCDIR}/libv8-2.a
 // #cgo LDFLAGS: -ldl -lm -lstdc++
 import "C"

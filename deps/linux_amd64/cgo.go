cfadf8b82b755fdb6900045b94afb33b27512341
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2026-05-19 23:37:03 -0300

    fix: use lld linker for Linux to handle V8 clang ELF objects
    V8's clang produces ELF objects with features GNU ld cannot parse
    ('unknown architecture'). Use LLVM's lld linker via -fuse-ld=lld
    for Linux targets. Install lld in CI.

--- deps/linux_amd64/cgo.go
+++ deps/linux_amd64/cgo.go
@@ -1,6 +1,6 @@
 package linux_amd64
 
-// #cgo LDFLAGS: -pthread
+// #cgo LDFLAGS: -pthread -fuse-ld=lld
 // #cgo LDFLAGS: ${SRCDIR}/libv8-0.a ${SRCDIR}/libv8-1.a ${SRCDIR}/libv8-2.a
 // #cgo LDFLAGS: -ldl -lm -lstdc++
 import "C"
